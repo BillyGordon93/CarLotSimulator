167e1d91f78af470a5696bfc5c28a5679b07d426
Billy Gordon <Billywgordon93@gmail.com>
2020-11-07 09:47:14 -0600

    Cleaned up Code

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarLotSimulator
+{
+    public class Car
+    {
+        public int Year { get; set; }
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public string EngineNoise { get; set; }
+
+        public string HonkNoise { get; set; }
+
+        public bool IsDriveable { get; set; }
+
+
+
+        // Parameterized Constructor
+        public Car(int year, string make, string model, string engineNoise, string honkNoise, bool isDriveable = false)
+        {
+            Year = year;
+            Make = make;
+            Model = model;
+            EngineNoise = engineNoise;
+            HonkNoise = honkNoise;
+            IsDriveable = isDriveable;
+        }
+
+        // Default Ctor
+        public Car()
+        {
+            CarLot.numberOfCars++;
+            //ClassName.StaticMember
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+    //public 
+
+
+
+
+
+
+
+
+
+
+
+}
+
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -17,17 +17,45 @@ namespace CarLotSimulator
             //Now that the Car class is created we can instanciate 3 new cars
             //Set the properties for each of the cars
             //Call each of the methods for each car
+            var myCar = new Car();
 
-            //*************BONUS*************//
+            myCar.Make = "honda";
+            myCar.Model = "civic";
+            myCar.Year = 2020;
+            myCar.HonkNoise = "loud";
+            myCar.EngineNoise = "noisy";
+            myCar.IsDriveable = true;
+            Console.WriteLine($"I own a {myCar.Year} {myCar.Make} {myCar.Model}");
+            Console.WriteLine($"Its Horn is very {myCar.HonkNoise}, the engine is slightly {myCar.EngineNoise}");
+            Console.WriteLine($"Its { myCar.IsDriveable} this car can be driven");
+            Console.WriteLine("Press enter to continue");
+            Console.ReadLine();
 
-            // Set the properties utilizing the 3 different ways we learned about, one way for each car
+            var alsoMyCar = new Car();
 
-            //*************BONUS X 2*************//
+            alsoMyCar.Make = "Chevrolet";
+            alsoMyCar.Model = "1500";
+            alsoMyCar.Year = 2013;
+            alsoMyCar.HonkNoise = "insert honk noise here";
+            alsoMyCar.EngineNoise = "Vroom Vroom";
+            alsoMyCar.IsDriveable = true;
+            Console.WriteLine($"I own a {alsoMyCar.Year} {alsoMyCar.Make} {alsoMyCar.Model}.");
+            Console.WriteLine($"Its { alsoMyCar.IsDriveable} that is can be driven although, the horn is very {alsoMyCar.HonkNoise}.");
+            Console.WriteLine($"Side Note I love to hear the engine go {alsoMyCar.EngineNoise}");
+            Console.WriteLine("Press enter to continue");
+            Console.ReadLine();
 
-            //Create a CarLot class
-            //It should have at least one property: a List of cars
-            //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
-            //At the end iterate through the list printing each of car's Year, Make, and Model to the console
+
+            var oldCar = new Car(2000, "Toyota", "Celica", "noisy", "honking", true);
+            Console.WriteLine($"I own a {oldCar.Year} {oldCar.Make} {oldCar.Model}");
+            Console.WriteLine($"It is {oldCar.IsDriveable}, this car is driveable.");
+            Console.WriteLine($"The engine is slightly {oldCar.EngineNoise}, but the {oldCar.HonkNoise} Function");
+            Console.WriteLine("works correctly.");
+            Console.WriteLine("Press enter to view amount of cars stored in the car lot currently.");
+            Console.ReadLine();
+            Console.WriteLine($"Number of cars currently in the car lot : {CarLot.numberOfCars}");
+
+
         }
-    }
+    }       
 }
